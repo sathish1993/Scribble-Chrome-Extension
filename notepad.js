442345ca472ff5d31471f09ada57d3697946c3d5
sathish1993 <sathish_kumar_93@yahoo.com>
2018-03-02 20:24:01 -0500

    updated uninstall url

--- notepad.js
+++ notepad.js
@@ -4,3 +4,5 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
    var newURL = "https://d1uhi7vkuhakrv.cloudfront.net/"
    chrome.tabs.create({ url: newURL });
 });
+
+chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSe2NMGTTupxCeDxykdhL9yPWJMCpXYwK7xr7imt5JBWxHhNDw/viewform?usp=sf_link")
